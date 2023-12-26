96d064e3d53250984060dcd82e35e837b689f11a
bxkay <ba@fast-forward-it.de>
2023-12-26 23:20:45 +0100

    TailwindCSS init

---
+++ src/js/scrollAnimation.js
@@ -0,0 +1,20 @@
+// Code für ScrollAnimations einbinden
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        // Falls Animation immer getriggert werden soll: 
+        // else {
+        //     entry.target.classList.remove('show');
+        // }
+    })
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => {
+    observer.observe(el)
+});
+
